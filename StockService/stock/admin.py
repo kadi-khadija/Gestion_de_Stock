e2ddafaa40d87e4b5b5fb98304e242192d066ffb
boukhedimiyasmine <boukhedimiyasmine.fs@univ-boumerdes.dz>
2025-12-01 09:08:12 +0100

    solving problems of stock service

--- StockService/stock/admin.py
+++ StockService/stock/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Stock
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("piece", "location", "quantity", "min_quantity", "last_updated")
